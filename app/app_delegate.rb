146d415cd2d3c0533e3965350273df0f966af3d6
Drew Carey Buglione <drewcbuglione@gmail.com>
2014-01-05 17:28:05 -0500

    Initialize persistent storage at launch

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,7 +2,13 @@ class AppDelegate < PM::Delegate
   status_bar true, animation: :none
 
   def on_load(app, options)
+    initialize_storage
     open HomeScreen.new(nav_bar: true)
   end
-  
+
+  def initialize_storage
+    {}.each do |key, value|
+      App::Persistence[key] = value if App::Persistence[key].nil?
+    end
+  end
 end
