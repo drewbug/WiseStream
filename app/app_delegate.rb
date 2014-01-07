aa279b7902db95193f9a63f4f1a9f81478de2f41
Drew Carey Buglione <drewcbuglione@gmail.com>
2014-01-07 12:50:27 -0500

    Add volume attributes to Streamers

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -8,7 +8,8 @@ class AppDelegate < PM::Delegate
   end
 
   def initialize_storage
-    {compass_enabled: false, clock_enabled: false}.each do |key, value|
+    {compass_enabled: false, compass_volume: 0.75,
+     clock_enabled: false, clock_volume: 0.75}.each do |key, value|
       App::Persistence[key] = value if App::Persistence[key].nil?
     end
   end
--- app/streamers/base_streamer.rb
+++ app/streamers/base_streamer.rb
@@ -3,11 +3,22 @@ class BaseStreamer
     @audio_context = audio_context
     @audio_context.on_ready do
       @gain_node = @audio_context.createGain
+      @gain_node.gain = App::Persistence[:"#{__key__}_volume"]
       @gain_node.connect(@audio_context.destination) if enabled?
       @oscillator = @audio_context.createOscillator
       @oscillator.connect @gain_node
       @oscillator.start(0)
     end
+  end
+
+  def volume
+    App::Persistence[:"#{__key__}_volume"]
+    @gain_node.gain if not @audio_context.locked
+  end
+
+  def volume=(value)
+    App::Persistence[:"#{__key__}_volume"] = value
+    @gain_node.gain = value if not @audio_context.locked
   end
 
   def enabled?
