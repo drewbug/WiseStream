1bad2bf975ee86ad8f63edfde20ad3195d06f281
Drew Carey Buglione <drewcbuglione@gmail.com>
2014-01-09 20:22:49 -0500

    Actual CompassScreen

--- app/screens/compass_screen.rb
+++ app/screens/compass_screen.rb
@@ -5,11 +5,20 @@ class CompassScreen < PM::FormotionScreen
     super.tap do |s|
       s.form.sections[0].rows[0].on_switch { |v| s.enabled_switched(v) }
       s.form.sections[1].rows[0].on_slide { |v| s.volume_slid(v) }
+      App.states[:compass_streamer].on_update do |heading|
+        s.form.sections[2].rows[0].value = heading
+      end
+      s.form.sections[3].rows[0].on_input { |v| s.freq180_input(v) }
+      s.form.sections[3].rows[1].on_input { |v| s.freq0360_input(v) }
     end
   end
 
   def on_load
     set_attributes self.view, { background_color: hex_color("EFEFF4") }
+  end
+
+  def will_dismiss
+    App.states[:compass_streamer].stop_updates
   end
 
   def table_data
@@ -21,6 +30,13 @@ class CompassScreen < PM::FormotionScreen
     hash[:sections][1] = { rows: [] }
     hash[:sections][1][:rows][0] = { title: 'Volume', key: :volume, type: :slider, range: (0..1), value: App::Persistence[:compass_volume] }
 
+    hash[:sections][2] = { rows: [] }
+    hash[:sections][2][:rows][0] = { title: 'Heading', key: :heading, type: :static }
+
+    hash[:sections][3] = { rows: [] }
+    hash[:sections][3][:rows][0] = { title: 'South (180°) Frequency', key: :freq180, type: :number, value: App::Persistence[:compass_freq180] }
+    hash[:sections][3][:rows][1] = { title: 'North (0°/360°) Frequency', key: :freq0360, type: :number, value: App::Persistence[:compass_freq0360] }
+
     return hash
   end
 
@@ -30,5 +46,13 @@ class CompassScreen < PM::FormotionScreen
 
   def volume_slid(value)
     App.states[:compass_streamer].volume = value
+  end
+
+  def freq180_input(value)
+    App.states[:compass_streamer].freq180 = value
+  end
+
+  def freq0360_input(value)
+    App.states[:compass_streamer].freq0360 = value
   end
 end
